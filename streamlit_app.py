fdf4e978219d69df1aec50208ee17fb9544e1ed6
Avaiz khan <121440674+khan-avaiz-snowflake@users.noreply.github.com>
2023-01-02 18:08:37 +0530

    Update streamlit_app.py
    new header added for advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+# new section to display fruityvice advice
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)

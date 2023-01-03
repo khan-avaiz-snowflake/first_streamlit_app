2c228987fc15ef656ff3742a1343a253e879493c
Avaiz khan <121440674+khan-avaiz-snowflake@users.noreply.github.com>
2023-01-03 18:58:17 +0530

    Update streamlit_app.py
    added new text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+selected_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('The user entered ', selected_fruit)
